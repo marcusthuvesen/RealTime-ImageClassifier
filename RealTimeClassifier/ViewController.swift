f5001a58b69ccdf661e994ff78466bf40a6ef935
Marcus Thuvesen <marcus.thuvesen@gmail.com>
2019-02-25 16:15:33 +0100

    Initial Commit

---
+++ RealTimeClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeClassifier
+//
+//  Created by Marcus thuvesen on 2019-02-25.
+//  Copyright © 2019 Marcus thuvesen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
